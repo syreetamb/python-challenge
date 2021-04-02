23758a59b6f0f614d7a1245d5ef9a47033a9d3c0
Syreeta Berry <mzberry1@hotmail.com>
2021-04-02 13:07:21 -0400

    #send

---
+++ PyBank/Resources/pybank.py
@@ -0,0 +1,16 @@
+import os
+import csv
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+#print(dir_path)
+os.chdir(dir_path)
+
+pybank_path = os.path.join('budget_data.csv')
+#print(pybank_path)
+
+with open(pybank_path, "r", encoding="utf-8") as pybankfile:
+
+    pybankreader = csv.reader(pybankfile, delimiter =',')
+
+    for row in pybankreader:
+        print(row)
