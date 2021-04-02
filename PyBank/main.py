9a1068423a4a7a9d086ee36ec23a88a7f55f4e36
Syreeta Berry <mzberry1@hotmail.com>
2021-04-02 13:20:50 -0400

    #send

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,15 +2,15 @@ import os
 import csv
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
+#print(dir_path)
 os.chdir(dir_path)
 
-pybank_path = os.path.join('Resources','budget_data.csv')
+pybank_path = os.path.join('budget_data.csv')
+#print(pybank_path)
 
 with open(pybank_path, "r", encoding="utf-8") as pybankfile:
 
     pybankreader = csv.reader(pybankfile, delimiter =',')
-
-    next(pybankreader)
 
     for row in pybankreader:
         print(row)
