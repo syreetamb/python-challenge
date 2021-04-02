c33d7b208f24f52decb6bf6a08a39674dded23bc
Syreeta Berry <mzberry1@hotmail.com>
2021-04-02 15:15:23 -0400

    #send

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,15 +2,28 @@ import os
 import csv
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-#print(dir_path)
 os.chdir(dir_path)
 
 pybank_path = os.path.join('Resources','budget_data.csv')
-#print(pybank_path)
+
+date = []
+profits_losses = []
+total_months = []
+total = []
+average_change = []
+greatest_increase_in_profits = []
+greatest_decrease_in_profits = []
+
 
 with open(pybank_path, "r", encoding="utf-8") as pybankfile:
 
     pybankreader = csv.reader(pybankfile, delimiter =',')
 
     for row in pybankreader:
-        print(row)
+        date.append(row[0])
+        profits_losses.append(row[1])
+
+
+        count = sum(row[0])
+        total_months.append(count)
+        print(total_months)
