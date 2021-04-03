60b632bbe55063761b92d3c9dd81f564a67d0a86
Syreeta Berry <mzberry1@hotmail.com>
2021-04-03 08:30:42 -0400

    #send

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,15 +2,16 @@ import os
 import csv
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-#print(dir_path)
 os.chdir(dir_path)
 
 pypoll_path = os.path.join('Resources','election_data.csv')
-#print(pypoll_path)
+
 
 with open(pypoll_path, "r", encoding="utf-8") as pypollfile:
 
     pypollreader = csv.reader(pypollfile, delimiter =',')
+
+    next(pypollreader)
 
     for row in pypollreader:
         print(row)
